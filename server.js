fe65d6cc162878718e9bd1a05f560ed676494b84
Slava Bezgachev <bezgachev@gmail.com>
2018-03-24 11:09:29 +0600

    Added dummy json server api

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
 
+const schema = require("./schema");
+
 const app = express();
 
 app.use("/graphql", expressGraphQL({
+  schema,
   graphiql: true
 }));
 
